4a534f97458286234bc399e0a9decf2ece3159f9
Robert Teets <teetsrobert@gmail.com>
2022-12-11 19:03:50 -0700

    feat: getDigits complete

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ const removeBlanks = (string) => {
     return returnString;
 };
 
+const getDigits = (string) => {
+    let returnString = "";
+    for (const char of string) {
+        if (!isNaN(char)) {
+            returnString += Number(char);
+        }
+    }
+    return returnString;
+};
+
 console.log(removeBlanks("This is a test"));
+console.log(getDigits("Thi3s 5is a6test"));
